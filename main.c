c976f6abee47c6c33a0da766f82442ef4ffbe9a1
Brian Downing <bdowning@lavos.net>
2016-05-03 00:48:06 -0500

    Serial, PID controller experiments

--- main.c
+++ main.c
@@ -4,6 +4,42 @@
 #include <string.h>
 #include <stdlib.h>
  
+#define BAUD 57600
+
+#include <util/setbaud.h>
+
+void uart_init(void) {
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
+}
+
+#include <stdio.h>
+
+int uart_putchar(char c, FILE *stream) {
+    if (c == '\n')
+        uart_putchar('\r', stream);
+    loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+    UDR0 = c;
+    return 0;
+}
+
+int uart_getchar(void) {
+    loop_until_bit_is_set(UCSR0A, RXC0); /* Wait until data exists. */
+    return UDR0;
+}
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
+                                         _FDEV_SETUP_WRITE);
+
 #define BLINK_DELAY_MS 1000
  
 #define DELAY 1
@@ -88,9 +124,104 @@ static void set_display(char *str1, char *str2)
     }
 }
 
+#define Kp 0.01
+#define Ki 0.002
+#define Kd 0.1
+
+#define LOW 0.40
+#define HIGH 1.00
+
+#define NUM_DIFFS 16
+
+#define VALVE_SPEED 3.5
+
+float
+process(register float val, int reset) {
+   static float buf[2];
+   register float tmp, fir, iir;
+   if (reset)
+       memset(buf, 0, sizeof(buf));
+   tmp= buf[0]; memmove(buf, buf+1, 1*sizeof(float));
+   // use 0.06745527388907191 below for unity gain at 100% level
+   iir= val * 0.0674552738890719;
+   iir -= 0.4128015980961885*tmp; fir= tmp;
+   iir -= -1.142980502539901*buf[0]; fir += buf[0]+buf[0];
+   fir += iir;
+   buf[1]= iir; val= fir;
+   return val;
+}
+
+static float actValvePos;
+
+static float integral;
+static float lastAdj;
+static float lastDiff;
+static float diffHistory[NUM_DIFFS];
+static float diffSum;
+static uint8_t diffIndex;
+
+float pid(float setPress, float press, float maxPress, int reset)
+{
+    float diff = setPress - press;
+    if (reset) {
+        integral = 0;
+        memset(diffHistory, 0, sizeof(diffHistory));
+        lastDiff = diff;
+        diffSum = 0;
+    }
+
+    float p = diff * Kp;
+    if (lastAdj > LOW - 0.1 && lastAdj < HIGH + 0.1)
+        integral += diff * Ki;
+    else
+        integral *= 0.95;
+
+    float diffDiff = diff - lastDiff;
+
+    /* diffSum -= diffHistory[diffIndex % NUM_DIFFS]; */
+    /* diffSum += diffHistory[diffIndex++ % NUM_DIFFS] = diffDiff; */
+    float smoothDiff = process(diffDiff, reset);
+    float d = smoothDiff * Kd;
+
+    float adj = LOW + p + integral + d;
+
+    lastAdj = adj;
+
+    if (adj > HIGH)
+        adj = HIGH;
+    if (adj < LOW)
+        adj = LOW;
+
+    float ticksToMax = 999;
+    if (smoothDiff < 0)
+        ticksToMax = (maxPress - press) / -smoothDiff;
+    if (ticksToMax > 999)
+        ticksToMax = 999;
+    float valveCloseTime = actValvePos * 10 * VALVE_SPEED;
+
+    printf("pres: %+5d adj: %+5d p: %+5d int: %+5d d: %+5d tts: %+5d vct: %+5d\n",
+           (int)(press * 100),
+           (int)(adj * 100),
+           (int)(p * 100),
+           (int)(integral * 100),
+           (int)(d * 100),
+           (int)(ticksToMax + 0.5),
+           (int)(valveCloseTime + 0.5));
+
+    if (valveCloseTime > ticksToMax)
+        adj = 0;
+
+    lastDiff = diff;
+
+    return adj;
+}
 
 int main (void)
 {
+    uart_init();
+    stdout = &mystdout;
+    printf("hi world!\n");
+
     /* set pin 5 of PORTB for output*/
     DDRB |= _BV(DDB5);
     DDRB |= _BV(DDB0);
@@ -175,15 +306,21 @@ int main (void)
 
         char buf[8];
         cli();
+        if (adc_count < 100) {
+            sei();
+            goto wait;
+        }
         long adc = (adc_sum * 100) / adc_count;
         adc_sum = 0;
         adc_count = 0;
         sei();
 
         int newCall = call;
+        int resetPid = 0;
         if (call == 0) {
             if (adc < lowMark)
                 newCall = 2;
+            resetPid = 1;
         } else if (call == 2) {
             if (adc > highMark)
                 newCall = 0;
@@ -209,6 +346,27 @@ int main (void)
             PORTB |= 0x03;
         }
 
+        float adj = 0;
+        char fbuf[10];
+        if (call == 1 || call == 2) {
+            adj = pid(set.pressure, (float)adc / scale, (float)highMark / scale, resetPid);
+            snprintf(fbuf, sizeof(fbuf), "%+9d", (int)(adj * 100));
+            memcpy(buf2 + 7, fbuf, 9);
+        }
+
+        if (adj > actValvePos) {
+            actValvePos += 1 / VALVE_SPEED / 10;
+            if (actValvePos > adj)
+                actValvePos = adj;
+        } else if (adj < actValvePos) {
+            actValvePos -= 1 / VALVE_SPEED / 10;
+            if (actValvePos < adj)
+                actValvePos = adj;
+        }
+
+        snprintf(fbuf, sizeof(fbuf), "%+6d", (int)(actValvePos * 100));
+        memcpy(buf2 + 0, fbuf, 6);
+
         ltoa(adc / scale, buf, 10);
         memcpy(buf1 + 2, "   ", 3);
         memcpy(buf1 + 2, buf, strlen(buf));
@@ -222,7 +380,8 @@ int main (void)
 
         /* memcpy(buf2 + 2, "      ", 6); */
         /* memcpy(buf2 + 2, buf, strlen(buf)); */
-        
+
+    wait:
         set_display(buf1, buf2);
 
         /* set pin 5 high to turn led on */
